bfb2b1482600089d62b85eab8fbb634f7ade7f91
Bert Dekker <info@bertdekker.net>
2024-11-22 00:05:23 +0100

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    # Vul de variabele posts met een QuerySet:
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # Geef deze variabele als parameter door aan de render functie:
+    return render(request, 'blog/post_list.html', {'posts': posts})
